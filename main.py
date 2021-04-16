45077f0882c1b32db801df26e4f19d3e1390f4c9
nadine <nadeen@pdx.edu>
2021-04-15 19:01:20 -0700

    resampling works, but the average goes up. do we want it to go down?

---
+++ main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import datetime
+
+# Enter in the CSVs you want to process:
+df_EV = pd.read_csv('EV1.csv', header=None)
+print("this is the first one")
+print(df_EV)
+
+# Will get rid of col labels later:
+df_EV.columns =['Timestamp', 'Power']
+
+
+# Make sure Timestamp type is datetime
+df_EV['Timestamp'] = df_EV['Timestamp'].astype('datetime64')
+print(len(df_EV['Timestamp']))
+
+# Set DateTime column as index
+df_EV.set_index('Timestamp', inplace=True)
+
+# 1D stands for 1 day offset
+df_EV = df_EV.resample('300s').mean() # 300 seconds = 5 mins
+
+print("this is the resampled one")
+print(df_EV)
+
+# Unindexing timestamp col:
+#df_EV['Timestamp'] = df_EV.index
+
+
+# Printing out sampled CSV with removed headers (col labels) and keeping the index (timestamp col):
+df_EV.to_csv('EV1_Resampled.csv', header=False, index=True)
