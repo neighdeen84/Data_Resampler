f967c43a6b04ff1c2d854635b6f3a59b389c5f73
nadine <nadeen@pdx.edu>
2021-05-11 18:52:01 -0700

    updated

--- main.py
+++ main.py
@@ -18,17 +18,14 @@ def iter_files(folder_path):
         # Enter in the CSVs you want to process:
         # Note: if the CSV has no header use: header = None
         # if it has a header use: header = 0
-        #print(os.getcwd())
         print(filename)
         df_EV = pd.read_csv(filename, header=None)
         df_2 =  pd.read_csv(filename, header=None)
 
 
-        #print(df_2)
         #### using the good timestamp:
         # Give labels & make sure Timestamp type is datetime
         df_2.columns =['Timestamp', 'Power']
-        #print(df_2)
 
         df_2['Timestamp'] = df_2['Timestamp'].astype('datetime64')
 
@@ -37,32 +34,24 @@ def iter_files(folder_path):
 
         # 900s stands for 15 min offset, add flag "closed='right' " to get it to do it down instead of up:
         df_2 = df_2.resample('900s').mean()
-        #print('this is df_2')
-        #print(df_2)
-        # Getting rid of the aliased power column:
 
+        # Getting rid of the aliased power column:
         del df_2['Power']
 
         # Unindexing timestamp col:
         df_2.reset_index(level=0, inplace=True)
-
-        #print(len(df_EV))
 
 
         #### Averaging:
         # Will get rid of col labels later:
         df_EV.columns =['Date', 'Power']
         del df_EV['Date']
-        #print('this is the length before:')
-        #print(len(df_EV))
+
 
         step = 900 # 15 mins
         df_EV = df_EV.groupby(df_EV.index // step).mean()
-        #print('this is the length after:')
-        #print(len(df_EV))
 
         dfT = pd.concat([df_2, df_EV], axis=1)
-        #dfT.replace(np.nan, 0, inplace=True)
 
         # Reduce to one decimal place:
         dfT.Power = df_EV.Power.round(2)
@@ -73,8 +62,11 @@ def iter_files(folder_path):
 
         # Ceil values less than 5760:
         dfT['Power'] = np.where( (dfT.Power > 0) & (dfT.Power < 5760) , 5760, dfT.Power)
-        #final_filename = os.join.path(resampled_dir,filname)
         # Printing out sampled CSV with removed headers (col labels) and keeping the index (timestamp col):
         dfT.to_csv(resampled_dir+'\\'+filename, header=False, index=False)
-        #print('made it here')
-iter_files('D:\\Git Projects\\Data_Resampler\\test\\')
+
+
+cwd = os.getcwd()
+data_dir = cwd + '\\data'
+
+iter_files(data_dir)
